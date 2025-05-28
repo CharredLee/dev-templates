d0cc93f59bad9440b2d83c38ddaba6a67974d866
CharredLee <31701136+CharredLee@users.noreply.github.com>
2025-05-28 17:38:31 -0400

    add node template

---
+++ templates/node/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based development environment for webdev using node";
+
+  inputs = {
+    nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?shallow=1&ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_22
+        ];
+      };
+    });
+  };
+}
